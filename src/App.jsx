c6a36687bf54a8467efd8c2f893548bf573b3658
barcelona <yuquansmart@126.com>
2024-10-07 18:47:10 +0800

    Q3 solved, Q1 add more traveller data variable

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,69 @@ const initialTravellers = [
   {
     id: 1, name: 'Jack', phone: 88885555,
     bookingTime: new Date(),
+    passport: 'EL3083605',
+    ticketType: 0,
+    // ticketType has provide food and not provide food, which is two type, using 0 and 1 to represent. 
+    emailAddress: '123@u.nus.edu',
   },
   {
     id: 2, name: 'Rose', phone: 88884444,
     bookingTime: new Date(),
+    passport: 'EA3083604',
+    ticketType: 0,
+    emailAddress: 'rose@u.nus.edu',
   },
   {
     id: 3, name: 'John', phone: 88883333,
     bookingTime: new Date(),
+    passport: 'EM3083603',
+    ticketType: 1,
+    emailAddress: 'johhhhh@u.nus.edu',
   },
   {
     id: 4, name: 'Panda', phone: 88882222,
     bookingTime: new Date(),
+    passport: 'ED3083602',
+    ticketType: 0,
+    emailAddress: 'Pandaspan@u.nus.edu',
   },
   {
     id: 5, name: 'Lindy', phone: 88881111,
     bookingTime: new Date(),
+    passport: 'EK3083601',
+    ticketType: 1,
+    emailAddress: 'Lindy@u.nus.edu',
   },
   {
     id: 6, name: 'Teenie', phone: 88887777,
     bookingTime: new Date(),
+    passport: 'EZ3083600',
+    ticketType: 1,
+    emailAddress: 'lijiateenie@u.nus.edu',
   },
   {
     id: 7, name: 'Weenie', phone: 88889999,
     bookingTime: new Date(),
+    passport: 'IL3083606',
+    ticketType: 0,
+    emailAddress: 'weenieThePooh@u.nus.edu',
   },
 ];
 
 
 function TravellerRow(props) {
   {/*Q3. Placeholder to initialize local variable based on traveller prop.*/ }
+  const { id, name, phone, bookingTime, passport, ticketType, emailAddress } = props.traveller;
   return (
     <tr>
       {/*Q3. Placeholder for rendering one row of a table with required traveller attribute values.*/}
+      <td>{id}</td>
+      <td>{name}</td>
+      <td>{phone}</td>
+      <td>{bookingTime.toLocaleString()}</td>
+      <td>{passport}</td>
+      <td>{ticketType}</td>
+      <td>{emailAddress}</td>
     </tr>
   );
 }
@@ -43,9 +73,10 @@ function TravellerRow(props) {
 function Display(props) {
 
   /*Q3. Write code to render rows of table, reach corresponding to one traveller. Make use of the TravellerRow function that draws one row.*/
+  const { travellers } = props;
 
   return (
-    <table className="bordered-table">
+    <table className="bordered-table" style={styles.table}>
       <thead>
         <tr>
           {/*Q3. Below table is just an example. Add more columns based on the traveller attributes you choose.*/}
@@ -53,10 +84,18 @@ function Display(props) {
           <th>Name</th>
           <th>Phone</th>
           <th>Booking Time</th>
+          <th>Passport</th>
+          <th>Ticket Type</th>
+          <th>Email Address</th>
         </tr>
       </thead>
       <tbody>
-        {/*Q3. write code to call the JS variable defined at the top of this function to render table rows.*/}
+        {/*Q3. write code to call the JS variable defined at the top of this function to render table rows.*/
+          travellers.map(traveller => (
+            <TravellerRow key={traveller.id} traveller={traveller} />
+          ))
+        }
+
       </tbody>
     </table>
   );
@@ -124,7 +163,7 @@ class Homepage extends React.Component {
     return (
       <div>
         {/*Q2. Placeholder for Homepage code that shows free seats visually.*/}
-        <h1>Homepage</h1>
+        {/* <h1>Homepage</h1> */}
         <p>Total Seats: {totalSeats}</p>
         <p>Occupied Seats: {occupiedSeats}</p>
         <p>Free Seats: {freeSeats}</p>
@@ -203,12 +242,12 @@ class TicketToRide extends React.Component {
         <h1>Ticket To Ride</h1>
         <h4>This is a reservation system for the high-speed railway network that runs from Singapore to Thailand</h4>
 
-        <div>
+        <div style={styles.navBar}>
           {/*Q2. Code for Navigation bar. Use basic buttons to create a nav bar. Use states to manage selection.*/}
-          <button onClick={() => this.setSelector(1)}>Homepage</button>
-          <button onClick={() => this.setSelector(2)}>Display Travellers</button>
-          <button onClick={() => this.setSelector(3)}>Add Traveller</button>
-          <button onClick={() => this.setSelector(4)}>Delete Traveller</button>
+          <button style={styles.button} onClick={() => this.setSelector(1)}>Homepage</button>
+          <button style={styles.button} onClick={() => this.setSelector(2)}>Display Travellers</button>
+          <button style={styles.button} onClick={() => this.setSelector(3)}>Add Traveller</button>
+          <button style={styles.button} onClick={() => this.setSelector(4)}>Delete Traveller</button>
         </div>
         <div>
           {/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
@@ -232,5 +271,40 @@ class TicketToRide extends React.Component {
 }
 
 const element = <TicketToRide />;
+
+
+const styles = {
+  table: {
+    width: '100%',
+    borderCollapse: 'collapse',
+    marginTop: '15px',
+  },
+  th: {
+    padding: '12px',
+    textAlign: 'left',
+    backgroundColor: '#f2f2f2',
+    borderBottom: '3px solid #ddd',
+  },
+  td: {
+    padding: '12px',
+    textAlign: 'left',
+    borderBottom: '3px solid #ddd',
+  },
+  navBar: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    marginBottom: '15px',
+    padding: '15px',
+    backgroundColor: '#C9E0F7',
+  },
+  button: {
+    padding: '5px 15px',
+    backgroundColor: '#6A6AEA',
+    color: 'black',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  }
+};
 
 ReactDOM.render(element, document.getElementById('contents'));
